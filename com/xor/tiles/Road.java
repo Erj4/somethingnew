ce292c5efb02cc8434466608aa4757f76d48e0ba
Erj4 <edward1999.jones@gmail.com>
2017-10-09 13:21:33 +0100

    started: com.xor.tile.Road.connect()

--- com/xor/tiles/Road.java
+++ com/xor/tiles/Road.java
@@ -1,10 +1,13 @@
 package com.xor.tiles;
-import javafx.scene.paint.Color;
 
+import java.util.Deque;
+import javafx.scene.paint.Color;
 public class Road extends Tile{
   static Color color = Color.DARKGRAY;
 
   public Road(int x, int y){
     super(x, y, color);
   }
+
+  public Deque<Road> connect(Tile tile, Map map); // should connect building by a series of road tiles to rest of city
 }
